af0a1300708979cc91c1093dd0abdf0beae517fc
Diego Reyes <dsebasrg057@gmail.com>
2025-03-07 14:28:44 -0600

    Archivo js actualizado: Se inluyó la función agregarAmigo

--- juegoAmigoSecreto.js
+++ juegoAmigoSecreto.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+console.log(amigos);
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if(nombre === ''){
+        alert("Ingresa un nombre");
+    }else 
+        amigos.push(nombre);
+
+    document.getElementById("amigo").value = '';
+    console.log(amigos);
+    
+    return;
+
+}
+
+//console.log(amigos);
